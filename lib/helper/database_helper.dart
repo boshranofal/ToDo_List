53fcea8ad30302c4958cffa108409735838de7d3
boshranofal <boshranofal66@gmail.com>
2025-02-26 20:06:27 +0200

    Helper for database operations.

--- lib/helper/database_helper.dart
+++ lib/helper/database_helper.dart
@@ -19,21 +19,29 @@ class DatabaseHelper {
   Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath(); 
     final path = join(dbPath, filePath); 
-    return await openDatabase(path, version: 1, onCreate: _createDB);  
+    return await openDatabase(path, version: 2, onCreate: _createDB, onUpgrade: _upgradeDB);  
   }
 
-  Future _createDB(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        description TEXT NOT NULL,
-        date TEXT NOT NULL
-      )
-    ''');
-  }
+ Future _createDB(Database db, int version) async {
+  await db.execute('''
+    CREATE TABLE tasks (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT NOT NULL,
+      date TEXT NOT NULL,
+     isCompleted INTEGER NOT NULL DEFAULT 0
+   
+    )
+  ''');
+}
 
-  // إضافة المهام
+  Future<void> _upgradeDB(Database db, int oldVersion, int newVersion) async {
+  if (oldVersion < 2) {
+    await db.execute('ALTER TABLE tasks ADD COLUMN isCompleted INTEGER DEFAULT 0');
+  }
+}
+ 
+
   Future<int> insertTask(TasksModel task) async {
     final db = await instance.database;
     return await db.insert('tasks', task.toMap());
@@ -60,6 +68,22 @@ class DatabaseHelper {
     final db = await instance.database;
     return await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
   }
+    Future<void> checkDatabaseSchema() async {
+    final db = await instance.database; 
+    final result = await db.rawQuery('PRAGMA table_info(tasks);');
+    print(result); 
+  }
+  Future<int> getCompletedTasksCount() async {
+  final db = await database;
+  final result = await db.rawQuery('SELECT COUNT(*) AS count FROM tasks WHERE isCompleted = 1');
+  return Sqflite.firstIntValue(result) ?? 0;
+}
+
+Future<int> getPendingTasksCount() async {
+  final db = await database;
+  final result = await db.rawQuery('SELECT COUNT(*) AS count FROM tasks WHERE isCompleted = 0');
+  return Sqflite.firstIntValue(result) ?? 0;
+}
 }
 
 
