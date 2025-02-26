b422c9edeefffd4ac31dcf6470539ffae2e65341
boshranofal <boshranofal66@gmail.com>
2025-02-26 20:09:18 +0200

    Initializes and runs the app.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:todo_list_app/presentation/screen/todo_home_screen.dart';
 import 'package:todo_list_app/provider/task_provider.dart';
 
-void main() {
+void main() async{
   runApp(
     ChangeNotifierProvider(
       create: (context) => TaskProvider(),
@@ -14,8 +14,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
